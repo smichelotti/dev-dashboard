dec198d48301fe566d87a37afe2b5e56474df75a
Steve Michelotti <steve.michelotti@microsoft.com>
2020-12-29 09:04:38 -0500

    added readme

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Dev Dashboard
+
+A C# [Gui.cs](https://github.com/migueldeicaza/gui.cs)-based developer dashboard. This was inspired by [Elijah Manor](https://elijahmanor.com/)'s [Terminal Dashboard with react-blessed](https://elijahmanor.com/blog/react-blessed).
+
+Currently includes:
+
+* Date/time
+* Weather
+* Trello integrattion
+* Local git repo visualization
+
+Remaining items:
+
+* Removing hard-coding of git integration
+* Adding any new utilities
+
+![screenshot](docs/screenshot.png)
--- src/DevDashboard/Components/GitInfo.cs
+++ src/DevDashboard/Components/GitInfo.cs
@@ -16,7 +16,7 @@ namespace DevDashboard.Components
         // TODO: refresh git log when branch changes (?)
         // TODO: general refresh on terminal focus (?)
 
-        private string gitDir = @"c:\development\ecp-core";
+        private string gitDir = @"c:\dev\ato-toolkit"; // sets initial (TODO: remove hard-coding)
         private Button selectBtn;
         private Label gitPath;
         private FrameView branches;
@@ -28,12 +28,38 @@ namespace DevDashboard.Components
 
         public override void Setup()
         {
-            this.selectBtn = new Button(x: 0, y: 0, text: "Select Repo");
+            this.selectBtn = new Button(x: 0, y: 0, text: "Select Repo")
+            {
+                Clicked = () =>
+                {
+                    var dialog = new Dialog("Specify repo directory");
+                    var txtDir = new TextField()
+                    {
+                        X = 1,
+                        Y = 1,
+                        Width = Dim.Percent(50)
+                    };
+                    var ok = new Button("OK")
+                    {
+                        Clicked = () =>
+                        {
+                            this.gitDir = txtDir.Text.ToString();
+                            Application.RequestStop();
+                            //this.RefreshGitLog();
+                        }
+                    };
+                    dialog.Add(txtDir);
+                    dialog.AddButton(ok);
+                    Application.Run(dialog);
+                    this.RefreshGitLog();
+                }
+            };
 
             this.gitPath = new Label($"({this.gitDir})")
             {
                 X = Pos.Right(this.selectBtn) + 3,
-                Y = 0
+                Y = 0,
+                Width = Dim.Fill()
             };
 
             this.branches = new FrameView("Branches")
@@ -63,12 +89,20 @@ namespace DevDashboard.Components
             this.gitLog = new Label(x: 0, y: 0, this.GetGitOutput("l -n 10"))
             {
                 Width = Dim.Fill(),
+                Height = Dim.Fill(),
                 ColorScheme = ColorSchemes.Get(Color.BrightYellow)
             };
             this.log.Add(this.gitLog);
 
 
             this.Frame.Add(this.selectBtn, this.gitPath, this.branches, this.log);
+        }
+
+        private void RefreshGitLog()
+        {
+            this.gitPath.Text = $"({this.gitDir})";
+            this.branchesList.Text = this.GetGitOutput("branch");
+            this.gitLog.Text = this.GetGitOutput("log --graph --decorate --pretty=oneline --abbrev-commit -n 10");
         }
 
         private string GetGitOutput(string gitArgs)
